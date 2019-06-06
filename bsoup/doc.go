8fc6d74058d341ef3f0fbdabc2f0ee3f188a2b90
Dustin Long <me@dustmop.io>
2019-06-06 17:36:33 -0400

    docs(bsoup): Additional bsoup method documentation, cleanup language

--- bsoup/doc.go
+++ bsoup/doc.go
@@ -7,22 +7,32 @@ in starlark
    types:
      SoupNode
        methods:
-			 find(name, attrs, recursive, string, **kwargs)
-					retrieve the first occurance of an element that matches arguments passed to find.
-					should work like [soup.find()](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find)
-			 find_all(name, attrs, recursive, string, limit, **kwargs)
-					retrieves all descendants that match arguments passed to find_all.
-					should work like [soup.find_all()](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all)
-			 attrs()
-					get a dictionary of element attributes
-					should work like [soup.attrs](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#attributes)
-			 contents()
-			 child()
-       parent()
-       next_sibling()
-       prev_sibling()
+         find(name, attrs, recursive, string, **kwargs)
+           retrieve the first occurance of an element that matches arguments passed to find.
+           works similarly to [node.find()](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find)
+         find_all(name, attrs, recursive, string, limit, **kwargs)
+           retrieves all descendants that match arguments passed to find_all.
+           works similarly to [node.find_all()](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all)
+         attrs()
+           get a dictionary of element attributes
+           works similarly to [node.attrs](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#attributes)
+         contents()
+           gets the list of children of an element
+           works similarly to [soup.contents](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#contents-and-children)
+         child()
+           gets a single child element with the given tag name
+           works like accessing a node [using its tag name](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#navigating-using-tag-names)
+         parent()
+           gets the parent node of an element
+           works like [node.parent](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#parent)
+         next_sibling()
+           gets the next sibling of an element
+           works like [node.next_sibling](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#next-sibling-and-previous-sibling)
+         prev_sibling()
+           gets the previous sibling of an element
+           works like [node.prev_sibling](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#next-sibling-and-previous-sibling)
    functions:
      parseHtml(html string) SoupNode
-       parseHTML parses html from a string, returning the root SoupNode
+       parseHtml parses html from a string, returning the root SoupNode
 */
 package bsoup
